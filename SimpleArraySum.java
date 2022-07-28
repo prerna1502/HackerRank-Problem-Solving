96e899d4a4bcddfbc8ca6c096e56ad99aac30f7c
Prerna Kumari <prer1506@gmail.com>
2022-07-28 22:32:19 +0530

    added

---
+++ SimpleArraySum.java
@@ -0,0 +1,14 @@
+public class SimpleArraySum {
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int output=0;
+        String[] input = br.readLine().split(" ");
+        for(String value:input)
+        {
+            output += Integer.parseInt(value);
+        }
+        System.out.println(output);
+    }
+}
