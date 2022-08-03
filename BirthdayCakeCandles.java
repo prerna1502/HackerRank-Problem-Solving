5c4ff7916e5d4df9b9ba588ec23d012c782e1632
Prerna Kumari <prer1506@gmail.com>
2022-08-03 11:31:16 +0530

    added

---
+++ BirthdayCakeCandles.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class BirthdayCakeCandles{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int max = 0;
+        int count = 0;
+        int h;
+        for(int i =0; i < n; i++){
+            h = sc.nextInt();
+            if(h > max){
+                count = 1;
+                max = h;
+            }else if(h == max){
+                count++;
+            }
+        }
+        System.out.println(count);
+}
+}
