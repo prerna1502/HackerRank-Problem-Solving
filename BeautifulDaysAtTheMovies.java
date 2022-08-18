e50e1a140afb87a86b87a4fb89eb9682e50d0deb
Prerna Kumari <prer1506@gmail.com>
2022-08-18 11:11:54 +0530

    Added

---
+++ BeautifulDaysAtTheMovies.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class BeautifulDaysAtTheMovies {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = input.nextInt();
+        int j = input.nextInt();
+        int k = input.nextInt();
+        
+        int beautifulDays = 0;
+        
+        for(int x = i; x <= j; x++)
+        {
+            StringBuilder day = new StringBuilder(String.valueOf(x));
+            int xReverse = Integer.parseInt(day.reverse().toString());
+                
+            if(Math.abs(x-xReverse) % k == 0)
+            {
+                beautifulDays++;
+            }
+        }
+        System.out.println(beautifulDays);
+    }
+}
